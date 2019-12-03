9c2a4b39b303449880ebbb9a3bc66f238555c583
Connor Woodahl <Woodahl@ECS.BAYLOR.EDU>
2019-12-03 17:58:59 -0600

    Votes now work

--- BCC/src/main/java/BNU/presentation/ReviewView.java
+++ BCC/src/main/java/BNU/presentation/ReviewView.java
@@ -32,6 +32,7 @@ public class ReviewView {
 	
 	public static void BuildReviewView(ReviewController controller, String content, String score, String reviewerID, String reviewID) {
 		controller.getModel().setScores(Integer.parseInt(score));
+		controller.getModel().setScores(Integer.parseInt(score));
 		
 		FileHandler fileHandler = null; 
 		try {
